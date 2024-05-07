d70c2e3f7357f76d2c58d9b1292f008e423900e4
or <romirov@gmail.com>
2024-05-08 00:17:29 +0300

    changed testMatch

--- jest.config.js
+++ jest.config.js
@@ -159,6 +159,10 @@ const config = {
   //   "**/?(*.)+(spec|test).[tj]s?(x)"
   // ],
 
+  testMatch: [
+    "**/src/?(*.)+(spec|test).[tj]s",
+  ],
+
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
   //   "/node_modules/"
